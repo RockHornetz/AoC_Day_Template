31986e6ab98ed93d24f18d86651e48d42b10af07
Pierre Hornez <hornez.pierre@gmail.com>
2024-01-05 11:09:02 -0500

    Update template

--- src/lib.rs
+++ src/lib.rs
@@ -26,38 +26,34 @@ impl Iterator for {{class_name}} {
 
 #[cfg(test)]
 mod tests {
+    use _common::AppError;
     use super::*;
     static TEST_PART1_STR: &str = include_str!("test_part1.txt");
     static TEST_PART2_STR: &str = include_str!("test_part2.txt");
 
     #[test]
-    fn test_part01() {
-        if let Some(mut aoc_day) = {{class_name}}::new(TEST_PART1_STR) {
-            assert_eq!(aoc_day.process_part_01(), 999)
-        }
+    fn test_part01() -> Result<(), AppError> {
+        let mut aoc_day = {{class_name}}::new(TEST_PART1_STR).ok_or(AppError::Error)?;
+        Ok(assert_eq!(aoc_day.process_part_01(), 999))
     }
     #[test]
-    fn test_part02() {
-        if let Some(mut aoc_day) = {{class_name}}::new(TEST_PART2_STR) {
-            assert_eq!(aoc_day.process_part_02(), 999)
-        }
+    fn test_part02() -> Result<(), AppError> {
+        let mut aoc_day = {{class_name}}::new(TEST_PART2_STR).ok_or(AppError::Error)?;
+        Ok(assert_eq!(aoc_day.process_part_02(), 999))
     }
     #[test]
-    fn test_part01_final() {
-        if let Some(mut aoc_day) = {{class_name}}::new(INPUT_STR) {
-            let result = aoc_day.process_part_01();
-            println!("Final Result Part01: {result}");
-            assert_eq!(result, 999)
-        }
+    fn test_part01_final() -> Result<(), AppError> {
+        let mut aoc_day = {{class_name}}::new(INPUT_STR).ok_or(AppError::Error)?;
+        let result = aoc_day.process_part_01();
+        println!("Final Result Part01: {result}");
+        Ok(assert_eq!(result, 999))
     }
 
     #[test]
-    fn test_part02_final() {
-        if let Some(mut aoc_day) = {{class_name}}::new(INPUT_STR) {
-            let result = aoc_day.process_part_02();
-            println!("Final Result Part02: {result}");
-            assert_eq!(result, 999)
-        }
+    fn test_part02_final() -> Result<(), AppError> {
+        let mut aoc_day = {{class_name}}::new(INPUT_STR).ok_or(AppError::Error)?;
+        let result = aoc_day.process_part_02();
+        println!("Final Result Part02: {result}");
+        Ok(assert_eq!(result, 999))
     }
-
 }
