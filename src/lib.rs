d1be7771e160d5a8ba1e63445101cf8132068a93
Pierre Hornez <hornez.pierre@gmail.com>
2024-01-05 16:31:06 -0500

    Update Template

--- src/lib.rs
+++ src/lib.rs
@@ -26,32 +26,36 @@ impl Iterator for {{class_name}} {
 
 #[cfg(test)]
 mod tests {
-    use _common::ApiError;
     use super::*;
-    static TEST_PART1_STR: &str = include_str!("test_part1.txt");
-    static TEST_PART2_STR: &str = include_str!("test_part2.txt");
+    use _common::anyhow;
+    use _common::anyhow::Context;
+    
+    static TEST_PART_01_STR: &str = include_str!("test_part_01.txt");
+    static TEST_PART_02_STR: &str = include_str!("test_part_02.txt");
 
     #[test]
-    fn test_part01() -> Result<(), ApiError> {
-        let mut aoc_day = {{class_name}}::new(TEST_PART1_STR).ok_or(ApiError::Error)?;
+    fn test_part_01() -> anyhow::Result<()> {
+        let mut aoc_day = {{class_name}}::new(TEST_PART_01_STR).context("Couldn't create")?;
         Ok(assert_eq!(aoc_day.process_part_01(), 999))
     }
+
     #[test]
-    fn test_part02() -> Result<(), ApiError> {
-        let mut aoc_day = {{class_name}}::new(TEST_PART2_STR).ok_or(ApiError::Error)?;
-        Ok(assert_eq!(aoc_day.process_part_02(), 999))
-    }
-    #[test]
-    fn test_part01_final() -> Result<(), ApiError> {
-        let mut aoc_day = {{class_name}}::new(INPUT_STR).ok_or(ApiError::Error)?;
+    fn test_part_01_final() -> anyhow::Result<()> {
+        let mut aoc_day = {{class_name}}::new(INPUT_STR).context("Couldn't create")?;
         let result = aoc_day.process_part_01();
         println!("Final Result Part01: {result}");
         Ok(assert_eq!(result, 999))
     }
 
     #[test]
-    fn test_part02_final() -> Result<(), ApiError> {
-        let mut aoc_day = {{class_name}}::new(INPUT_STR).ok_or(ApiError::Error)?;
+    fn test_part_02() -> anyhow::Result<()> {
+        let mut aoc_day = {{class_name}}::new(TEST_PART_02_STR).context("Couldn't create")?;
+        Ok(assert_eq!(aoc_day.process_part_02(), 999))
+    }
+
+    #[test]
+    fn test_part_02_final() -> anyhow::Result<()> {
+        let mut aoc_day = {{class_name}}::new(INPUT_STR).context("Couldn't create")?;
         let result = aoc_day.process_part_02();
         println!("Final Result Part02: {result}");
         Ok(assert_eq!(result, 999))
